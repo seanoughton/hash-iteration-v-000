9f73a4a0bc49e12ea43a3e9e45c0443268fd2236
Sean Oughton <seanoughton@gmail.com>
2018-01-30 00:06:32 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
@@ -10,5 +10,7 @@ def select_winner(passengers)
   # add the code snippet here!
   winners = ""
   passengers.each do |suite,name|
+    if suite == :suite_a
+    end
   end
 end
